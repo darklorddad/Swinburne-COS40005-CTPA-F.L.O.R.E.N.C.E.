1f0a6c0526ba1e4be8c6454fa655d53d2171ec71
darklorddad <danieltiong000@gmail.com>
2025-09-20 09:24:05 +0800

    refactor: update state class names and opacity values

--- flutteriment-three/lib/screens/home/components/search_field.dart
+++ flutteriment-three/lib/screens/home/components/search_field.dart
@@ -14,7 +14,7 @@ class SearchField extends StatelessWidget {
         onChanged: (value) {},
         decoration: InputDecoration(
           filled: true,
-          fillColor: kSecondaryColor.withOpacity(0.1),
+          fillColor: kSecondaryColor.withAlpha(26),
           contentPadding:
               const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           border: searchOutlineInputBorder,
