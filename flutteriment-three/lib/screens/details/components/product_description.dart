1eb008c2aef0c069b525e6799a31013a0ee338e4
darklorddad <danieltiong000@gmail.com>
2025-09-20 09:28:45 +0800

    fix: correct model import paths to use lowercase filenames

--- flutteriment-three/lib/screens/details/components/product_description.dart
+++ flutteriment-three/lib/screens/details/components/product_description.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../constants.dart';
-import '../../../models/Product.dart';
+import '../../../models/product.dart';
 
 class ProductDescription extends StatelessWidget {
   const ProductDescription({
